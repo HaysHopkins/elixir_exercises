8dbe07924c0da4d04d628c03a4e62a1347d57dfd
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2017-12-28 15:21:22 -0500

    Add split,filter,take

--- h_list_recursion_5.exs
+++ h_list_recursion_5.exs
@@ -1,16 +1,29 @@
 defmodule HListRecursion5 do
-  def all?(list, fun), do: _all?(list, true, fun)
+  def  all?(list, fun),                       do: _all?(list, true, fun)
   defp _all?(_, applies, _) when not applies, do: false
-  defp _all?([], _, _), do: true
-  defp _all?([h|t], applies, fun), do: _all?(t, fun.(h), fun)
+  defp _all?([], _, _),                       do: true
+  defp _all?([h|t], _, fun),                  do: _all?(t, fun.(h), fun)
 
-  def each(list, fun), do: _each(list, fun, [])
-  defp _each([], _, acc), do: Enum.reverse acc
+  def  each(list, fun),        do: _each(list, fun, [])
+  defp _each([], _, acc),      do: Enum.reverse acc
   defp _each([h|t], fun, acc), do: _each(t, fun, [fun.(h) | acc])
 
-  # def split(), do: _split()
+  def  split(list, amount),        do: _split(list, amount, [])
+  defp _split([], _, acc),         do: { Enum.reverse(acc), [] }
+  defp _split(list, 0, acc),       do: { Enum.reverse(acc), list }
+  defp _split([h|t], amount, acc), do: _split(t, amount-1, [h|acc])
 
-  # def filter(), do: _filter()
+  def  filter(list, fun),   do: _filter(list, fun, acc)
+  defp _filter([], _, acc), do: acc
+  defp _filter([h|t], fun, acc) do
+    if fun.(h) do
+      _filter(t, fun, [h|acc])
+    else
+      _filter(t, fun, acc)
+    end
+  end
 
-  # def take(), do: _take()
+  def  take(list, amount),        do: _take(list, amount, [])
+  defp _take(_, 0, acc),          do: Enum.reverse acc
+  defp _take([h|t], amount, acc), do: _take(t, amount-1, [h|acc])
 end
