d65c5cf8fc06f50c2d8204f02cec282af46a61dd
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2017-12-28 14:50:36 -0500

    Refactor fib

--- h_fibonacci.exs
+++ h_fibonacci.exs
@@ -1,25 +1,16 @@
 defmodule HFibonacci do
-  def fibonacci(count) when not is_integer(count) do
-    IO.puts "Must enter an integer"
+
+  def start(n) do
+    start = :os.system_time(:seconds)
+    fibonacciNumber = fibonacci(n)
+    finish = :os.system_time(:seconds)
+    totalTime = finish - start
+    IO.puts "The fibonacciNumber was #{fibonacciNumber}."
+    IO.puts "Fibonacci for number placed #{n} finished in #{totalTime} seconds!"
   end
 
-  def fibonacci(count) when count < 0 do
-    IO.puts "Integer must be greater than 0"
-  end
-
-  def fibonacci(count) when count == 1 do
-    1
-  end
-
-  def fibonacci(count) do
-    fibonacci(count-1, 1, 1)
-  end
-
-  defp fibonacci(0, cur_num, last_num) do
-    cur_num
-  end
-
-  defp fibonacci(count, cur_num, last_num) do
-    fibonacci(count-1, cur_num + last_num, cur_num)
-  end
+  def fibonacci(count) when count < 0, do: raise "Must use a non-negative number"
+  def fibonacci(count), do: fibonacci(count, 1, 0)
+  defp fibonacci(0, _, result), do: result
+  defp fibonacci(count, next, result), do: fibonacci(count-1, next + result, next)
 end
