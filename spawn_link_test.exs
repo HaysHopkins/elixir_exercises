03ed98abd3d3799b04064964450b40afab21655b
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2018-01-07 12:35:46 -0500

    Spawn link exercise 3

---
+++ spawn_link_test.exs
@@ -0,0 +1,23 @@
+defmodule SpawnLinkTest do
+  import :timer, only: [sleep: 1]
+  def run do
+    spawn_link(SpawnLinkTest, :test, [self()])
+
+    sleep 500
+
+    loop_receive()
+  end
+
+  def loop_receive do
+    receive do
+      msg ->
+        IO.puts "Message: #{inspect msg}"
+        loop_receive
+    after 600 -> IO.puts "Done receiving messages"
+    end
+  end
+
+  def test(parent) do
+    send parent, "Message from the child!"
+  end
+end
